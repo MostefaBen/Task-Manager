a773fa2dd468020e838480ec9c8dd8ec6ae81257
MostefaBen <bennaceurmostefa@gmail.com>
2016-12-30 16:23:13 +0100

    add some code

---
+++ Task Manager.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void main() {
+    
+	system("COLOR 9");
+	cout << endl << "1 :  Activate the task manager" << endl
+         << endl << "2 :  Disable the task manager " << endl
+		 << endl << "3 :  Exit." << endl;
+		 
+	cout << endl << "Put your choice : " << endl;
+	char c;	 
+	cin >> c;        
+    system("cls");
+	
+	switch (c) {
+	
+        case '1':system("REG ADD hkcu\\Software\\Microsoft\\Windows\\CurrentVersion"
+                        "\\policies\\system /v DisableRegistryTools /t reg_dword /d 0");
+            break;
+			
+        case '2':system("REG ADD hkcu\\Software\\Microsoft\\Windows\\CurrentVersion"
+                        "\\policies\\system /v DisableTaklMgr /t reg_dword /d 1");
+            break;
+			
+		case '3':system("EXIT");
+            break;	
+			
+        default:cout << "Error, choose again" << endl;
+    }
+    
+}
